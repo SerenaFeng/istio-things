134f03f819cc7645e4e965a4c7bfa16507713aca
feng.xiaowei <feng.xiaowei@zte.com.cn>
2021-02-26 19:05:45 +0800

    leverage local sleep.yaml for multi-cluster

--- xcluster/3-example.sh
+++ xcluster/3-example.sh
@@ -6,5 +6,5 @@ k c ns sample
 k l ns sample istio-injection=enabled
 k t a helloworld.yaml -l service=helloworld
 k t a helloworld.yaml -l version=v${CLUSTER}
-k t a ../samples/sleep/sleep.yaml
+k t a sleep.yaml
 
